43f4e42b0bbc27e740464ecfea3d9ff945d43797
Aman Jha <101969746+AmannJha@users.noreply.github.com>
2023-11-27 14:44:45 +0000

    datatypes

---
+++ 01_bsics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict";
+
+console.log(2
+    +
+    2);//code redability should be high
+
+    console.log("aman jha");
+
+    let name = "aman jha"
+    let age = 21
+    let isloggedin = true
+
+    /*  
+***********************primitive data types in js*******************************
+number => 2 to the power
+
+string => this is string
+
+boolean => this returns tru/false
+
+undefined =>when we cannot assign the value in the variable
+
+null => when we give no value in this variable (like mere pass Ek Bhi rupaye Nahin Hai)
+
+symbol =>JavaScript Symbols are a new type of primitive data type introduced in the ES6 version of the language. They are used to represent unique values that can be used as identifiers or keys in objects. They are also used to create private properties and methods in classes.
+
+
+biggint => to store big integer values that are too big to be represented by a normal JavaScript Number .
+
+
+***********************non-primitive data types in js.*******************************
+
+object => An object is a complex data type that allows us to store collections of data. 
+    */
+
+console.log(typeof "aman jha")//strin
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
