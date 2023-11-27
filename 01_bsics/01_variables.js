bc38b692f26613aeb18c31c535c4f8cb129fdf4f
Aman Jha <101969746+AmannJha@users.noreply.github.com>
2023-11-27 14:45:39 +0000

    variables

---
+++ 01_bsics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 21457
+let accountEmail = "amanjha9990@gmail.com"
+var accountAss = "amanjha9990"
+
+// accountId = 2156
+accountAss = "ama9990"
+accountEmail = "jhaaman9990a@mail.com"
+
+console.log(accountId);
+console.table([accountAss,accountEmail,accountId]);
